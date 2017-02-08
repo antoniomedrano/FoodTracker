7cd31589bae517675c27867ae1031f6ac78ea408
Antonio Medrano <tonedabass@gmail.com>
2017-02-07 18:09:40 -0800

    begin adding a subclass of UIStackView

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,15 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Antonio Medrano on 2/7/17.
+//  Copyright © 2017 Antonio Medrano. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+
+
+}
