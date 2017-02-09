1a2605481881830e5891b1f8121428f453857cad
Antonio Medrano <tonedabass@gmail.com>
2017-02-09 13:45:50 -0800

    Started new "Meal" class to be used as a data model

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,22 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Antonio Medrano on 2/9/17.
+//  Copyright © 2017 Antonio Medrano. All rights reserved.
+//
+
+import UIKit
+
+
+class Meal {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+}
+
+
