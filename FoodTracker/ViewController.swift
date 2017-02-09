00a669c1c0f5f71ae8ba1c07781a80460f242eb0
Antonio Medrano <tonedabass@gmail.com>
2017-02-08 19:06:19 -0800

    cleaned up UI, changed default photo to be "No photo selected"

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -70,9 +70,5 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         imagePickerController.delegate = self
         present(imagePickerController, animated: true, completion: nil)
     }
-  
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }    
 }
 
