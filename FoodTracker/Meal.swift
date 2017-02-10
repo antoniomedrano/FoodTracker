c80aba9d4c9db404b8fae975c15751e76c5f38cd
Antonio Medrano <tonedabass@gmail.com>
2017-02-09 17:08:37 -0800

    created unit tests for the meal class, everything checks out great!

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -22,8 +22,13 @@ class Meal {
     
     init?(name: String, photo: UIImage?, rating: Int) {
         
-        // Initialization should fail if there is no name or if the rating is negative.
-        if name.isEmpty || rating < 0  {
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
             return nil
         }
     
