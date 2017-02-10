6f047a877e730b0b9753c79ec241f867f7a612b4
Antonio Medrano <tonedabass@gmail.com>
2017-02-09 16:43:48 -0800

    Created initial Meal object

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -16,6 +16,24 @@ class Meal {
     var name: String
     var photo: UIImage?
     var rating: Int
+
+    
+    //MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0  {
+            return nil
+        }
+    
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+    
+
     
 }
 
