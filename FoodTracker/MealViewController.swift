56f3e7b94907c8805245d996b1c9c4d1ffd34aa8
Antonio Medrano <tonedabass@gmail.com>
2017-02-20 18:29:29 -0800

    Added "cancel" button functionality

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -73,6 +73,9 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     }
     
     //MARK: Navigation
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
     
     // This method lets you configure a view controller before it's presented.
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
